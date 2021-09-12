443d7b3ddd5befa2b2124b273113eb47cb90ff06
thaianh97 <58674701+thaianh97@users.noreply.github.com>
2021-09-12 16:27:22 +0700

    update Network project

--- app/src/main/java/com/example/helloworld/model/Content.java
+++ app/src/main/java/com/example/helloworld/model/Content.java
@@ -1,4 +1,22 @@
 package com.example.helloworld.model;
 
 public class Content {
+    private String description;
+    private String url;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- app/src/main/java/com/example/helloworld/network/APIManager.java
+++ app/src/main/java/com/example/helloworld/network/APIManager.java
@@ -1,4 +1,18 @@
 package com.example.helloworld.network;
 
+import com.example.helloworld.model.Item;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface APIManager {
+    String SERVER_URL = "https://api-demo-anhth.herokuapp.com/";
+
+    @GET("data.json")
+    Call<Item> getItemData();
+
+    @GET("datas.json")
+    Call<List<Item>> getListData();
 }
